58db16cdd1e89e04c5f11712805cd46c06c6d5e3
Andrew Dedik <swipe440@gmail.com>
2026-02-13 11:18:26 +0300

    to be or not to be

--- testy.cpp
+++ testy.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 main(){
-system("chcp 65001 > nul"); // comand for Russian lenguage
+system("chcp 65001 > nul"); // comand for Russian lenguage, I don't know how it works 
     cout << "To be or not to be";
 
     return 0;
